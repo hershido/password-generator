4084358fd5e985dd73db8aee2ee8e7647b6aff8c
hershido <hershido@gmail.com>
2022-12-14 10:09:41 +0200

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { PasswordGenerator } from './components/PasswordGenerator';
 import './GlobalVariables.scss';
 
 function App() {
+   console.log('test')
    return (
       <div className='App'>
+         
          <PasswordGenerator />
       </div>
    );
